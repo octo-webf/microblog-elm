25a7a895e16329ccef4c548aff24695942dd4114
orocancourt <orocancourt@octo.com>
2017-02-24 00:04:01 +0100

    npm/yarn/webpack/elm setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import Elm from './App/App.elm';
+
+const element = document.getElementById('elm-app');
+Elm.App.embed(element);
---
+++ webpack.development.config.babel.js
@@ -0,0 +1,43 @@
+import webpack from 'webpack';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+export default {
+  entry: {
+    app: './src/index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        use: {
+          loader: 'elm-webpack-loader',
+          options: {
+            verbose: true,
+            warn: true
+          }
+        }
+      }
+    ],
+    noParse: /\.elm$/
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ],
+  devtool: 'eval',
+  devServer: {
+    contentBase: './src/',
+    inline: true,
+    hot: true,
+    historyApiFallback: true,
+    open: true
+  }
+};
