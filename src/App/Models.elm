3e2b8e0f3b8b05ce7fb1fdf7e55ed2bd1696a1e7
Anass Oumiri <aoumiri@octo.com>
2017-03-10 13:13:13 +0100

    Create the message view and the /login route

--- src/App/Models.elm
+++ src/App/Models.elm
@@ -2,14 +2,17 @@ module Models exposing (..)
 
 import Routing.Router exposing (Route)
 import Views.Login exposing (LoginModel, defaultLoginModel)
+import Views.Messages exposing (MessagesModel, defaultMessagesModel)
 
 type alias Model =
     { route : Route
+    , messages : MessagesModel
     , login : LoginModel
     }
 
 initialModel : Route -> Model
 initialModel route =
     { route = route
+    , messages = defaultMessagesModel
     , login = defaultLoginModel
     }
--- src/App/View.elm
+++ src/App/View.elm
@@ -6,11 +6,14 @@ import Models exposing (Model)
 import Routing.Router exposing (Route(..))
 import Errors.ErrorViews exposing (notFoundView)
 import Views.Login exposing (loginView)
+import Views.Messages exposing (messagesView)
 
 
 view : Model -> Html Msg
 view model =
     case model.route of
+        MessagesRoute ->
+            messagesView model.messages
         LoginRoute ->
             loginView model.login
         NotFoundRoute ->
